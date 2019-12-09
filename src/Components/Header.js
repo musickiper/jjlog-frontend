26021e661c84dfef20d268efb9da86b670200d1c
James Jung <leedo01219@gmail.com>
2019-12-09 13:01:17 -0500

    Fixed Log Out button color on Header component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import {Link} from "react-router-dom";
-import {MdSearch} from "react-icons/all";
-import {gql} from 'apollo-boost';
-import {useMutation} from "react-apollo-hooks";
+import { Link } from "react-router-dom";
+import { MdSearch } from "react-icons/all";
+import { gql } from "apollo-boost";
+import { useMutation } from "react-apollo-hooks";
 
 const Container = styled.div`
   display: flex;
@@ -26,9 +26,9 @@ const Search = styled(Link)`
 
 const Title = styled(Link)`
   margin: 30px;
-  font-size: 3.0rem;
+  font-size: 3rem;
   font-weight: bold;
-  transition: transform .2s;
+  transition: transform 0.2s;
   &:hover {
     transform: scale(1.5);
   }
@@ -49,6 +49,7 @@ const LogOut = styled.div`
   margin: 30px;
   cursor: pointer;
   border: 1px solid ${props => props.theme.blueColor};
+  color: ${props => props.theme.blueColor};
   padding: 16px;
   border-radius: ${props => props.theme.borderRadius};
   &:hover {
@@ -58,28 +59,31 @@ const LogOut = styled.div`
 `;
 
 const LOG_OUT = gql`
-    mutation logUserOut {
-        logUserOut @client
-    }
+  mutation logUserOut {
+    logUserOut @client
+  }
 `;
 
-const Header = ({isLoggedIn}) => {
-    const logOut = useMutation(LOG_OUT)[0];
+const Header = ({ isLoggedIn }) => {
+  const logOut = useMutation(LOG_OUT)[0];
 
-    return (
-        <Container>
-            <Search to="/search"><MdSearch/></Search>
-            <Title to="/">J J L O G</Title>
-            {!isLoggedIn
-                ? <LogIn to={"/login"}>Log In</LogIn>
-                : <LogOut onClick={logOut}>Log Out</LogOut>
-            }
-        </Container>
-    );
+  return (
+    <Container>
+      <Search to="/search">
+        <MdSearch />
+      </Search>
+      <Title to="/">J J L O G</Title>
+      {!isLoggedIn ? (
+        <LogIn to={"/login"}>Log In</LogIn>
+      ) : (
+        <LogOut onClick={logOut}>Log Out</LogOut>
+      )}
+    </Container>
+  );
 };
 
 Header.propTypes = {
-    isLoggedIn: PropTypes.bool.isRequired
+  isLoggedIn: PropTypes.bool.isRequired
 };
 
 export default Header;
