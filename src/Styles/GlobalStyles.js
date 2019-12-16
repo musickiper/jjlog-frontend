6c00cabe801e97a9e31e178ff5002585016ad892
James Jung <leedo01219@gmail.com>
2019-12-16 15:58:50 -0500

    Removed reset-style module beacuse it made many default tags didn't work

--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -1,16 +1,14 @@
 import { createGlobalStyle } from "styled-components";
-import reset from "styled-reset";
 
 export default createGlobalStyle`
-    ${reset};
-    @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap');
+    @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&display=swap');
     * {
         box-sizing: border-box;
     }
     body {
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
         background-color:${props => props.theme.bgColor};
         color:${props => props.theme.blackColor};
-        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
     }
     a {
         color:${props => props.theme.blueColor};
