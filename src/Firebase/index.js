38fbbdfd5ff637c45e6a3307025c099abcf7606a
JamesJung <leedo01219@gmail.com>
2019-12-15 22:32:00 -0500

    Update index.js

--- src/Firebase/index.js
+++ src/Firebase/index.js
@@ -3,14 +3,14 @@ import "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCq-iZt8p5wR4PsSEuyFELUGpqWMiteBOA",
-    authDomain: "jjlog-b9206.firebaseapp.com",
-    databaseURL: "https://jjlog-b9206.firebaseio.com",
-    projectId: "jjlog-b9206",
-    storageBucket: "jjlog-b9206.appspot.com",
-    messagingSenderId: "1088185027327",
-    appId: "1:1088185027327:web:e7ecc794f7841db4041aca",
-    measurementId: "G-V7N8DJJX1B"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_DOMAIN,
+    databaseURL: process.env.FIREBASE_DB_URL,
+    projectId: process.env.FIREBASE_PRJ_ID,
+    storageBucket: process.env.FIREBASE_BUCKET,
+    messagingSenderId: process.env.MSG_SEND_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASURE_ID
 };
 
 // Initialize Firebase
