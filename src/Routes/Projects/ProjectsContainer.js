d3fb0251ec6aaea8ba9bf875fda2a8e477a8cc4b
James Jung <leedo01219@gmail.com>
2019-12-16 11:27:29 -0500

    Fixed bugs on loading projects

--- src/Routes/Projects/ProjectsContainer.js
+++ src/Routes/Projects/ProjectsContainer.js
@@ -1,36 +1,43 @@
 import React from "react";
-import {useQuery} from "react-apollo-hooks";
-import queryString from 'query-string';
+import { useQuery } from "react-apollo-hooks";
+import queryString from "query-string";
 import ProjectsPresenter from "./ProjectsPresenter";
-import {ALL_POSTS, SEARCH_POST} from "./ProjectsQueries";
+import { ALL_POSTS, SEARCH_POST } from "./ProjectsQueries";
 
-const ProjectsContainer = ({match, location}) => {
-    const {term} = queryString.parse(location.search);
-    const {category} = match.params;
-    const getAllPostsQuery = useQuery(ALL_POSTS);
-    const searchPost = useQuery(SEARCH_POST, {variables: {term: term || ""}});
-    const {data, loading, error} = term ? searchPost : getAllPostsQuery;
+const ProjectsContainer = ({ match, location }) => {
+  const { term } = queryString.parse(location.search);
+  const { category } = match.params;
 
-    if (error) {
-        console.error(error);
-        throw Error(error);
-    }
+  const getAllPostsQuery = useQuery(ALL_POSTS);
+  const searchPost = useQuery(SEARCH_POST, { variables: { term: term || "" } });
+  const { data, loading, error } = term ? searchPost : getAllPostsQuery;
 
-    if (loading) {
-        return <div>loading...</div>;
-    } else if (term) {
-        const {searchPost} = data;
-        const posts = category ? searchPost.filter(post => {
-            return post.categories.filter(c => c.title === category).length !== 0;
-        }) : searchPost;
-        return <ProjectsPresenter posts={posts}/>;
-    } else {
-        const {allPosts} = data;
-        const posts = category ? allPosts.filter(post => {
-            return post.categories.filter(c => c.title === category).length !== 0;
-        }) : allPosts;
-        return <ProjectsPresenter posts={posts}/>
-    }
+  if (error) {
+    console.error(error);
+    return "";
+  }
+
+  if (loading || !data) {
+    return <div>loading...</div>;
+  }
+
+  if (term) {
+    const { searchPost } = data;
+    const posts = category
+      ? searchPost.filter(post => {
+          return post.categories.filter(c => c.title === category).length !== 0;
+        })
+      : searchPost;
+    return <ProjectsPresenter posts={posts} />;
+  } else {
+    const { allPosts } = data;
+    const posts = category
+      ? allPosts.filter(post => {
+          return post.categories.filter(c => c.title === category).length !== 0;
+        })
+      : allPosts;
+    return <ProjectsPresenter posts={posts} />;
+  }
 };
 
 export default React.memo(ProjectsContainer);
